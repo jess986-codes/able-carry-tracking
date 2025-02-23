6fe86e2646c8e1adfd2c2267c113d4954166d3dc
Jessica Yuen <jess986.codes@gmail.com>
2025-02-23 21:31:27 +1100

    add script and dependencies to track able carry bag stock status

---
+++ able-carry-scheduled-script.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo "the script is running"
+CD /D path-to-program
+"path-to-python.exe" ".\thirteen-restock.py"
+@echo "finished running"
---
+++ constants.py
@@ -0,0 +1,12 @@
+# urls
+THIRTEEN_BLACK_URL = 'https://ablecarry.com/products/thirteen-daybag?variant=39872111247473'
+THIRTEEN_OLIVE_URL = 'https://ablecarry.com/products/thirteen-daybag?variant=39872111345777'
+THIRTEEN_RED_URL = 'https://ablecarry.com/products/thirteen-daybag?variant=39872111280241'
+
+# colours
+BLACK = "black"
+OLIVE = "olive"
+RED = "red"
+
+#text
+RESTOCKED_TEXT = 'add to cart'
